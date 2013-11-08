a791dc7eafe62d2ad75b9605a02e49b27db5f5a4
Erick Navarro <erickxls@gmail.com>
2013-11-08 15:48:49 -0500

    added views for manage product groups

--- www/js/movies-app.js
+++ www/js/movies-app.js
@@ -1,4 +1,7 @@
 $(function(){
+
+	var DOMAIN = "http://royalpioneers.com";
+
 	function setContentSize() {
 		$('.swiper-content').css({
 			height: $(window).height()-$('.swiper-nav').height()
@@ -39,4 +42,99 @@ $(function(){
 			}	
 		}
 	}
+
+	// choose product group view
+
+	function chooseProductGroup (callback_url) {
+        var $page = $('#product-group-list-page');
+        var list = $page.find('ul');
+        var html = '';
+        var url = DOMAIN + '/mobile/product-groups/';
+        var prev = '#product-detail-page';
+        var list = $page.find('ul').html('');
+        $.ajax({
+            method: 'GET',
+            url: url,
+            dataType: 'json',
+            success: function (data) {
+            	if (data.status === 'success') {
+            		var len = data.groups.length, i, group;
+	                for (i=0; i<len; i++) {
+	                    group = data.groups[i];
+	                    html += '<li data-icon="false"><a href="'+ prev +'" data-id="' + group.id + '" class="group-for-choose">' + group.name + '</a></li>'
+	                }
+	                list.append(html);
+	                list.trigger('create');
+	                $('.group-for-choose').on('click', groupForChoose);
+	                document.location.href = callback_url;
+            	} else {
+            		alert('no groups found');
+            	}
+            }
+        })
+    }
+
+    $('#add-to-group-btn').on('click', function (e) {
+    	e.preventDefault();
+    	var callback_url = $(this).attr('href');
+    	chooseProductGroup(callback_url);
+    })
+
+    // When make a click in group name, this is saved and the user will be redirected to previous page
+
+    function groupForChoose (e) {
+    	e.preventDefault();
+        var $this = $(this);
+        var url = DOMAIN + '/mobile/add-product-to-group/';
+        $.ajax({
+            url: url,
+            type: 'POST',
+            data: {
+                productModelId: window.localStorage['productModelId'],
+                groupId: $this.data('id')
+            },
+            dataType: 'json',
+            success: function (data) {
+                if (data.status === 'success') {
+                    delete window.localStorage['productModelId'];
+                    document.location.href = $this.attr('href');
+                } else {
+                	alert(data.status);
+                }
+            }
+        });
+    }
+
+    $('.group-for-choose').on('click', groupForChoose);
+
+	$('.product-link').on('click', function (e) {
+		e.preventDefault();
+		var $this = $(this);
+		var id = $this.data('id');
+		window.localStorage['productModelId'] = id;
+		var url = '';
+		var $detailPage = $('#product-detail-page');
+		$detailPage.find('h1').text('fooo');
+		var detail = '#product-detail-page';
+		document.location.href = detail;
+		// TODO: add ajax for get product detail by ajax
+	});
+
+	$('#form-add-group').on('submit', function (e) {
+		e.preventDefault();
+		var $this = $(this);
+		$.ajax({
+			url: DOMAIN + '/mobile/product-group-create/',
+			type: 'POST',
+			data: {
+				name: $('#form-group-name').val()
+			},
+			dataType: 'json',
+			success: function (data) {
+				if (data.status === 'success') {
+					document.location.href = '#product-group-list-page';
+				}
+			}
+		});
+	});
 })
