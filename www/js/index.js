e87bbb2ad048b6bcb6061bcc6fa907fedf50f839
Jonathan <jonathancg90@gmail.com>
2013-11-13 18:04:51 -0500

    Remove images

--- www/js/index.js
+++ www/js/index.js
@@ -1,7 +1,9 @@
 
 var app = {
     getDomain: function(){
-        return "http://127.0.0.1:8000";
+        //return "http://127.0.0.1:8000";
+        return "http://192.168.1.6:8008";
+
     },
     initialize: function() {
         this.bindEvents();
