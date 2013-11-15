5b0b7c97cc9f0e966c68bcd69ccf3221f4771376
John Paul <john.cfmr.2009@gmail.com>
2013-11-14 19:08:03 -0500

    Fixed styles and effects in mobile

--- www/js/main_mobile.js
+++ www/js/main_mobile.js
@@ -193,12 +193,13 @@ function init() {
         }
     }
     function saveClientStorage() {
+        debugger;
         var clientSelected = JSON.parse(localStorage.getItem('clientSelected'));        
         if(clientSelected.products == ''){
             cleanClientSelected();
             $.mobile.navigate("#pagina11");
         }
-        else{
+        else if(clientSelected.products != ''){
             //validar si esta repetido
             var index = getArrayIndexClientsSelected().indexOf(clientSelected.id);
             if(index !== -1){
@@ -207,6 +208,7 @@ function init() {
             else{
                 storageClients.push(clientSelected);
             }
+            $.mobile.navigate("#pagina12");
         }
 
     }
