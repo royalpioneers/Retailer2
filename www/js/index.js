2fe334e3aec89e069ee447eefc381f579db6bef5
Jonathan <jonathancg90@gmail.com>
2013-11-28 17:44:13 -0500

    change domain

--- www/js/index.js
+++ www/js/index.js
@@ -2,7 +2,7 @@
 var app = {
     getDomain: function() {
         //return "http://roypi.com"
-        return "http://127.0.0.1:8000";
+        return "http://192.168.1.6:8000";
     },
     initialize: function() {
         this.bindEvents();
