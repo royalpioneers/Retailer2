e5c133b01aa6743514a757aacae124253830ae52
hubert-egues <hubert.rpd@gmail.com>
2013-12-30 13:50:56 -0500

    add validation for Buyer without stores

--- www/js/factory/buyer_inventory.js
+++ www/js/factory/buyer_inventory.js
@@ -43,6 +43,10 @@ var BuyerInventoryFactory = function(urls, token, cache) {
 	};
 	
 	factory.get_all = function(store, handler, cache) {
+		value = window.localStorage.getItem(factory.storage_id_inventory);
+		if (value == 'undefined') {
+			window.localStorage.removeItem(factory.storage_id_inventory);
+		}
 		var list = JSON.parse(window.localStorage.getItem(factory.storage_id_inventory));
 		if ((factory.cache || cache) && list != null) {
 			return handler(factory.get_items_from_store(store, list));
@@ -68,14 +72,18 @@ var BuyerInventoryFactory = function(urls, token, cache) {
 	
 	factory.get_stores = function() {
 		/* call after "factory.get_all" method */
-		return JSON.parse(window.localStorage.getItem(factory.storage_id_inventory));
+		stores = JSON.parse(window.localStorage.getItem(factory.storage_id_inventory));
+		if (typeof(stores) == 'undefined' || stores == null) {
+			stores = [];
+		}
+		return stores;
 	};
 	
 	factory.get_items_from_store = function(store_id, stores) {
-		if (typeof(stores) == 'undefined' ) {
+		if (typeof(stores) == 'undefined') {
 			stores = factory.get_stores();
 		}
-		if (typeof(store_id) == 'undefined' || store_id == null)  {
+		if ((typeof(store_id) == 'undefined' || store_id == null) && stores.length > 0)  {
 			return stores[0].items_list;
 		} else {
 			store_id = parseInt(store_id);
