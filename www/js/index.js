f0f43b7fd552b96aa1d18025db5bae967ade1f94
john <john.cfmr.2009@gmail.com>
2014-01-31 17:56:11 -0500

    progress

--- www/js/index.js
+++ www/js/index.js
@@ -2,7 +2,7 @@
 var app = {
     getDomain: function() {
         // return "http://roypi.com";
-        return "http://localhost:8002";
+        return "http://192.168.1.7:8002";
     },
     initialize: function() {
         this.bindEvents();
