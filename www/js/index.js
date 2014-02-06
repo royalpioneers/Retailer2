d5c1c03d459b6bd85d137e682f08fd58d507f73e
john <john.cfmr.2009@gmail.com>
2014-02-06 14:37:49 -0500

    progress invoice

--- www/js/index.js
+++ www/js/index.js
@@ -2,8 +2,8 @@
 var app = {
     getDomain: function() {
         //return "http://roypi.com";
-        // return "http://192.168.1.7:8000";
-        return "http://127.0.0.1:8000";
+         return "http://192.168.1.7:8000";
+        //return "http://127.0.0.1:8000";
     },
     initialize: function() {
         this.bindEvents();
