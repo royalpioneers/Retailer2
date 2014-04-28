d6607cde19e32371b26bd1fd71d24dec9c8d654a
john <john.cfmr.2009@gmail.com>
2014-04-28 16:28:09 -0500

    progress

--- www/js/index.js
+++ www/js/index.js
@@ -1,9 +1,9 @@
 
 var app = {
     getDomain: function() {
-        //return "http://royalpioneers.com/";
+        return "http://royalpioneers.com/";
         //return "http://192.168.0.26:8000";
-        return "http://roypi.com/";
+        //return "http://roypi.com/";
     },
     initialize: function() {
         this.bindEvents();
